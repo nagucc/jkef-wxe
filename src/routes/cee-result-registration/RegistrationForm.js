fddbfa26569ffaa7cddf82076f673f8e6736dec2
brealisty <294120771@qq.com>
2016-05-30 23:05:18 +0800

    add view of registrationform and amend weui's class 'weui_label'

--- src/routes/cee-result-registration/RegistrationForm.js
+++ src/routes/cee-result-registration/RegistrationForm.js
@@ -1,10 +1,86 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 
-class RegistrationForm extends React.Component {
-  render() {
+var RegistrationForm = React.createClass({
+  render: function () {
     return (
-      <div>code gose here</div>
+      <div>
+        <div className="hd">
+          <h1 className="page_title">学生信息</h1>
+        </div>
+        <div className="bd">
+          <FormList data={this.props.data}/>
+        </div >
+      </div>  
+    )
+  }
+});
+
+var FormList = React.createClass({
+  render: function () {
+    var formNodes = this.props.data.map(function (structure) {
+      return (
+        <FormInput key={structure.id} name={structure.name} content={structure.content}/>
+      );
+    });
+    return (
+      <form className="formList infoForm" role="form" action="/api/fundinfo" method="post">
+        <div className="weui_cells_title">请认真填写你的信息资料</div>
+        <div className="weui_cells weui_cells_form">
+          <div className="weui_cell">
+            <div className="weui_cell_hd">
+              <label className="weui_label">性别</label>
+            </div>
+            <div className="weui_cell_bd weui_cell_primary">
+              <input type="radio" name="sex" value="男"/>
+              <label>男</label>
+              <input type="radio" name="sex" value="女"/>
+              <label>女</label>
+            </div>
+          </div>
+          <div className="weui_cell">
+            <div className="weui_cell_hd">
+              <label className="weui_label">类别</label>
+            </div>
+            <div className="weui_cell_bd weui_cell_primary">
+              <input type="radio" name="type" value="文"/>
+              <label>文</label>
+              <input type="radio" name="type" value="理"/>
+              <label>理</label>
+              <input type="radio" name="type" value="艺体"/>
+              <label>艺体</label>
+            </div>
+          </div>
+          {formNodes}
+          <FormButton />
+        </div>
+      </form>
     );
   }
-}
+});
+
+var FormInput = React.createClass({
+  render: function () {
+    return (
+      <div className="formInput weui_cell">
+        <div className="weui_cell_hd">
+          <label className="weui_label">{this.props.content}</label>
+        </div>
+        <div className="weui_cell_bd weui_cell_primary">
+          <input className="weui_input" name={this.props.name} type="text" placeholder={"请输入"+this.props.content}/>
+        </div>
+      </div>
+    );
+  }
+});
+var FormButton = React.createClass({
+  render: function () {
+    return (
+      <div className="weui_btn_area">
+        <button type="submit" className="weui_btn weui_btn_primary">确定</button>
+        <a href="#" className="weui_btn weui_btn_default">返回</a>
+      </div>
+    );
+  }
+});
+
 export default RegistrationForm;
--- src/routes/cee-result-registration/index.js
+++ src/routes/cee-result-registration/index.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import RegistrationForm from './RegistrationForm';
 
+var structure = [
+    {
+      "id": 1,
+      "content": "姓名",
+      "name": "name"
+    },
+    {
+      "id": 2,
+      "content": "身份证",
+      "name": "id"
+    },
+    {
+      "id": 3,
+      "content": "毕业学校",
+      "name": "graduation"
+    },
+    {
+      "id": 4,
+      "content": "高考分数",
+      "name": "grade"
+    },
+    {
+      "id": 5,
+      "content": "录取学校",
+      "name": "university"
+    },
+    {
+      "id": 6,
+      "content": "录取专业",
+      "name": "major"
+    },
+    {
+      "id": 7,
+      "content": "录取层次",
+      "name": "degree"
+    }
+  ];
+
 export default {
 
   path: '/cee-result-registration',
 
   async action() {
-    return <RegistrationForm />;
+    return <RegistrationForm data={structure}/>;
   },
 };
