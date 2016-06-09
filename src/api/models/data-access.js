eba091fbf6085f5cdae58cdb62f93b7b57568964
na57 <na57@qq.com>
2016-06-09 19:25:54 +0800

    fix

--- src/api/models/data-access.js
+++ src/api/models/data-access.js
@@ -11,7 +11,7 @@ export const useStatByProject = cb => useCollection(mongoUrl, STAT_BY_PROJECT, c
 export const useStatByYear = cb => useCollection(mongoUrl, STAT_BY_YEAR, cb);
 
 export const computeStatByProject = async () =>
-  new Promise(resolve => useAcceptors(async col => {
+  new Promise((resolve, reject) => useAcceptors(async col => {
     const map = function () { // eslint-disable-line
       if (this.records) {
         this.records.forEach(function (record) { // eslint-disable-line
@@ -31,20 +31,28 @@ export const computeStatByProject = async () =>
         count += val.count;
         lastUpdated = Math.max(lastUpdated, +val.lastUpdated);
       });
-      return { amount, count, lastUpdated };
+      // mongodb 中不支持shorthand
+      return {
+        amount: amount, // eslint-disable-line object-shorthand
+        count: count, // eslint-disable-line object-shorthand
+        lastUpdated: lastUpdated, // eslint-disable-line object-shorthand
+      };
     };
 
-    await col.mapReduce(map, reduce, {
-      out: {
-        replace: STAT_BY_PROJECT,
-      },
-    });
-    resolve();
+    try {
+      await col.mapReduce(map, reduce, {
+        out: {
+          replace: STAT_BY_PROJECT,
+        },
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
   }));
 
-
 export const computeStatByYear = async () =>
-  new Promise((resolve) => {
+  new Promise((resolve, reject) => {
     useAcceptors(async col => {
       const map = function () { // eslint-disable-line
         if (this.records) {
@@ -65,14 +73,23 @@ export const computeStatByYear = async () =>
           count += val.count;
           lastUpdated = Math.max(lastUpdated, +val.lastUpdated);
         });
-        return { amount, count, lastUpdated };
+        // mongodb 中不支持shorthand
+        return {
+          amount: amount, // eslint-disable-line object-shorthand
+          count: count, // eslint-disable-line object-shorthand
+          lastUpdated: lastUpdated, // eslint-disable-line object-shorthand
+        };
       };
-      await col.mapReduce(map, reduce, {
-        out: {
-          replace: STAT_BY_YEAR,
-        },
-      });
-      resolve();
+      try {
+        await col.mapReduce(map, reduce, {
+          out: {
+            replace: STAT_BY_YEAR,
+          },
+        });
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
     });
   });
 
@@ -92,10 +109,7 @@ export const getStatByYear = () =>
     });
   });
 
-export const findAcceptors = ({ text, year, project, projections, skip, limit } = {
-  skip: 0,
-  limit: 20,
-}) => {
+export const findAcceptors = ({ text, year, project, projections, skip = 0, limit = 20 } = {}) => {
   let condition = { isDeleted: { $ne: true } };
   if (text) {
     var reg = new RegExp(text); // eslint-disable-line vars-on-top, no-var
@@ -138,36 +152,37 @@ export const findAcceptors = ({ text, year, project, projections, skip, limit } 
   });
 };
 
-const mustBeProvided = () => {
-  throw new Error('参数必须提供');
-};
 
 /*
 idCard.number作为唯一标识字段，添加或更新acceptor
  */
-export const addAcceptor = async ({ name, isMale, phone, idCard, userid } = {
-  name: mustBeProvided(),
-  isMale: null,
-  phone: null,
-  idCard: mustBeProvided(),
-}) => new Promise((resolve, reject) => {
-  useAcceptors(async col => {
-    try {
-      const result = await col.insertOne({ name, phone, isMale, idCard, userid });
-      if (result.insertedCount === 1) {
-        const doc = await col.findOne({ _id: result.insertedId });
-        resolve(doc);
-      } else reject(result);
-    } catch (e) {
-      reject(e);
-    }
+export const addAcceptor = async ({ name, isMale, phone, idCard, userid }) =>
+  new Promise((resolve, reject) => {
+    useAcceptors(async col => {
+      if (!name) {
+        reject('姓名不能为空');
+        return;
+      }
+      if (!idCard || !idCard.type || !idCard.number) {
+        reject('证件类型和号码不能为空');
+        return;
+      }
+      try {
+        const result = await col.updateOne({ 'idCard.number': idCard.number },
+          { name, phone, isMale, idCard, userid },
+          { upsert: true });
+        if (result.result.ok === 1) resolve(result.result.upserted[0]._id);
+        else reject(result.result);
+      } catch (e) {
+        reject(e);
+      }
+    });
   });
-});
 
 /*
 通过idCard.number找到相应的acceptor
  */
-export const findAcceptorByIdCardNumber = async idCardNumber =>
+export const findByIdCardNumber = async idCardNumber =>
   new Promise((resolve, reject) => {
     useAcceptors(async col => {
       try {
@@ -198,16 +213,49 @@ export const update = async (_id, newData) =>
   new Promise((resolve, reject) => useAcceptors(async col => {
     try {
       const { name, phone, idCard, isMale, userid } = newData;
-      let query;
+      let query = {};
       if (name) query = { ...query, name };
       if (phone) query = { ...query, phone };
-      if (isMale) query = { ...query, isMale };
-      if (idCard) query = { ...query, idCard };
+      if (isMale !== undefined) query = { ...query, isMale };
+      // if (idCard) query = { ...query, idCard };
+      if (idCard) {
+        if (idCard.type) {
+          Object.assign(query, {
+            'idCard.type': idCard.type,
+          });
+        }
+        if (idCard.number) {
+          Object.assign(query, {
+            'idCard.number': idCard.number,
+          });
+        }
+      }
       if (userid) query = { ...query, userid };
-      const result = await col.updateOne({ _id }, {
+      const result = await col.updateOne({ _id: new ObjectId(_id) }, {
         $set: query,
       });
       resolve(result);
+    } catch (e) {
+      reject(e);
+    }
+  }));
+
+export const deleteAcceptor = id => new Promise((resolve, reject) =>
+  useAcceptors(async col => {
+    try {
+      const result = await col.updateOne({ _id: new ObjectId(id) }, {
+        $set: { isDeleted: true },
+      });
+      resolve(result);
+    } catch (e) {
+      reject(e);
+    }
+  }));
+
+export const remove = async _id =>
+  new Promise((resolve, reject) => useAcceptors(async col => {
+    try {
+      resolve(await col.remove({ _id: new ObjectId(_id) }));
     } catch (e) {
       reject(e);
     }
