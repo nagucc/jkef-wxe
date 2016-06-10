bdad10cbd7c8d26195c029ed44961708b40319c2
na57 <na57@qq.com>
2016-06-10 18:29:35 +0800

    使用redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import listAcceptors from './listAcceptors';
 import me from './wxe-auth';
 import acceptors from './acceptors';
+import {reducer as formReducer} from 'redux-form';
 
 export default combineReducers({
   listAcceptors,
   me,
   acceptors,
+  form: formReducer,
 });
--- src/routes/acceptors/registration/Registration.js
+++ src/routes/acceptors/registration/Registration.js
@@ -2,11 +2,11 @@
 eslint-disable react/prop-types
  */
 import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
 import { CellsTitle, CellHeader, CellBody,
   Form, FormCell, Input, Select, Button, Msg } from 'react-weui';
 import NeedSignup from '../../../components/NeedSignup';
 import CheckRoles from '../../../components/CheckRoles';
+import { reduxForm } from 'redux-form'
 
 /*
 受赠者登记表
@@ -28,31 +28,13 @@ export class RegistrationComponent extends React.Component {
     ui: PropTypes.object,
     me: PropTypes.object,
   };
-  // static defaultProps = {
-  //   acceptor: {
-  //     idCard: {},
-  //     name: 'xxx',
-  //   },
-  // };
-  // componentWillReceiveProps(nextProps) {
-  //   // console.log(nextProps.acceptor)
-  //   // this.setState(nextProps.acceptor);
-  // }
   async componentDidMount() {
 
-    // console.log(this.props.acceptor)
     const { error, dispatch, unauthorized } = this.props;
 
     let acceptor = this.props.acceptor;
     if (this.props.fetchById) {
-      // this.props.fetchById().then(data => {
-      //   // this.setState(data);
-      //   dispatch(this.props.showRegistration(data));
-      // }, result => {
-      //
-      // })
       acceptor = await this.props.fetchById();
-      this.setState(acceptor);
       dispatch(this.props.showRegistration(acceptor));
     }
 
@@ -60,46 +42,18 @@ export class RegistrationComponent extends React.Component {
       dispatch(unauthorized());
       return;
     }
-    // console.log(this.props.acceptor.idCard)
-    // this.setState(this.props.acceptor);
-    // if (this.props.error) return;
-    // console.log('dispathcEvnet.........', this.props.acceptor)
-    // const event = new MouseEvent('change', {
-    //   view: window,
-    //   bubbles: true,
-    // });
-    // document.getElementById('idCardType').dispatchEvent(event);
-    document.getElementById('idCardType').value = acceptor.idCard.type;
-    this.setState({
-      idCard: {
-        type: acceptor.idCard.type,
-        number: this.state.idCard.number,
-      },
+    const event = new MouseEvent('change', {
+      view: window,
+      bubbles: true,
     });
-    switch (acceptor.idCard.type) {
-      case '组织机构代码证':
-        dispatch(this.props.setIdCardTypeGroup());
-        break;
-      case '身份证':
-        dispatch(this.props.setIdCardTypePerson());
-        break;
-      default:
-        dispatch(this.props.howRegistration());
-    }
+    document.getElementById('idCardType').dispatchEvent(event);
   }
   render() {
-    const { ui, dispatch, setUserRole, acceptor, error } = this.props;
-    // console.log('render..................', acceptor, this.state)
+    const { ui, dispatch, setUserRole, acceptor, error, fields } = this.props;
 
     // 处理证件类型改变时的事件
     const typeChanged = e => {
-      // console.log(this.state.idCard.number)
-      this.setState({
-        idCard: {
-          type: e.target.value,
-          number: this.state.idCard.number,
-        },
-      });
+      fields.idCard.type.onChange(e);
       const { setIdCardTypeGroup, setIdCardTypePerson, showRegistration } = this.props;
       switch (e.target.value) {
         case '组织机构代码证':
@@ -114,36 +68,13 @@ export class RegistrationComponent extends React.Component {
     };
     // 处理提交按钮的点击事件
     const submit = () => {
-      // const { isManager } = this.props.me.roles;
-      // const regPerson = ui.isMale.visiable;
       const { action } = this.props;
-
-      // // 设置userid的值
-      // let userid = '';
-      // if (isManager) userid = document.getElementById('userid').value;
-      //
-      // // 设置isMale的值
-      // let isMale = '';
-      // if (regPerson) isMale = document.getElementById('isMale').value;
-      //
-      // // 准备好需要PUT的所有数据
-      // const data = {
-      //   userid,
-      //   name: document.getElementById('name').value,
-      //   phone: document.getElementById('phone').value,
-      //   isMale,
-      //   idCard: {
-      //     type: document.getElementById('idCardType').value,
-      //     number: document.getElementById('idCardNumber').value,
-      //   },
-      // };
       action(this.state).then(acc => {
         window.location = `/acceptors/detail/${acc._id}`;
       }, result => {
         alert(`操作失败：${JSON.stringify(result.msg)}`); // eslint-disable-line no-alert
       });
     };
-
     // 返回组件
     return (
       <div className="progress">
@@ -174,26 +105,16 @@ export class RegistrationComponent extends React.Component {
                       value: '组织机构代码证',
                       label: '组织机构代码证',
                     }]}
-                      id="idCardType"
+                      {...fields.idCard.type}
                       onChange={typeChanged}
-                      defaultValue={acceptor.idCard.type}
+                      id="idCardType"
                     />
                   </CellBody>
                 </FormCell>
                 <FormCell>
                   <CellHeader>证件号码</CellHeader>
                   <CellBody>
-                    <Input
-                      placeholder="请输入证件号码" id="idCardNumber"
-                      type="text"
-                      value={this.state.idCard.number}
-                      onChange={e => this.setState({
-                        idCard: {
-                          type: this.state.idCard.type,
-                          number: e.target.value,
-                        },
-                      })}
-                    />
+                    <Input placeholder="请输入证件号码" {...fields.idCard.number} />
                   </CellBody>
                 </FormCell>
               </Form>
@@ -206,10 +127,7 @@ export class RegistrationComponent extends React.Component {
                 <FormCell>
                   <CellHeader>帐号</CellHeader>
                   <CellBody>
-                    <Input placeholder="企业号帐号" id="userid"
-                      value={this.state.userid}
-                      onChange={e => this.setState({ userid: e.target.value })}
-                    />
+                    <Input placeholder="企业号帐号" {...fields.userid} />
                   </CellBody>
                 </FormCell>
               </Form>
@@ -222,10 +140,7 @@ export class RegistrationComponent extends React.Component {
                 <FormCell>
                   <CellHeader>名称</CellHeader>
                   <CellBody>
-                    <Input placeholder="姓名或组织名称" id="name"
-                      value={this.state.name}
-                      onChange={e => this.setState({ name: e.target.value })}
-                    />
+                    <Input placeholder="姓名或组织名称" {...fields.name} />
                   </CellBody>
                 </FormCell>
                 {
@@ -242,9 +157,7 @@ export class RegistrationComponent extends React.Component {
                           value: false,
                           label: '女',
                         }]}
-                          id="isMale"
-                          value={this.state.isMale}
-                          onChange={e => this.setState({ isMale: e.target.value })}
+                          {...fields.isMale}
                         />
                       </CellBody>
                     </FormCell>
@@ -253,10 +166,7 @@ export class RegistrationComponent extends React.Component {
                 <FormCell>
                   <CellHeader>手机号码</CellHeader>
                   <CellBody>
-                    <Input placeholder="请输入手机号码" id="phone"
-                      value={this.state.phone}
-                      onChange={e => this.setState({ phone: e.target.value })}
-                    />
+                    <Input placeholder="请输入手机号码" {...fields.phone} />
                   </CellBody>
                 </FormCell>
               </Form>
@@ -275,12 +185,14 @@ export class RegistrationComponent extends React.Component {
 }
 
 const mapStateToProps = state => {
-  // console.log('connnect...........', state.acceptors.registration.data)
-  // console.log(state.acceptors.registration.ui);
   return {
     ...state.acceptors.registration,
     me: state.me,
     acceptor: state.acceptors.registration.data,
+    initialValues: state.acceptors.registration.data,
   };
 };
-export default connect(mapStateToProps)(RegistrationComponent);
+export default reduxForm({
+  form: 'registration',
+  fields: ['userid', 'idCard.number', 'idCard.type', 'name', 'phone', 'isMale'],
+}, mapStateToProps)(RegistrationComponent);
