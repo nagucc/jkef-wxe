9ef3ef437c5ad64a0421a1ce26395b4dc9a6fcec
na57 <na57@qq.com>
2016-06-19 17:22:58 +0800

    设置全局error

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,16 @@ import me from './wxe-auth';
 import acceptors from './acceptors';
 import { reducer as formReducer } from 'redux-form';
 import stat from './stat';
+import { FETCH_FAILED } from '../constants';
+
+const error = (state = null, action) => {
+  switch (action.type) {
+    case FETCH_FAILED:
+      return action.error;
+    default:
+      return null;
+  }
+};
 
 export default combineReducers({
   listAcceptors,
@@ -11,4 +21,5 @@ export default combineReducers({
   acceptors,
   form: formReducer,
   stat,
+  error,
 });
