2c2ece7dd21bf7d46e7b75938f443891aa36cd87
na57 <na57@qq.com>
2016-06-23 14:17:54 +0800

    stat/by-project 删除hd，请求数据时加入toast显示

--- src/reducers/stat/by-project.js
+++ src/reducers/stat/by-project.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { STAT_BY_PROJECT } from '../../constants';
+import { STAT_BY_PROJECT, FETCHING } from '../../constants';
 
 const data = (state = [], action) => {
   switch (action.type) {
@@ -10,6 +10,18 @@ const data = (state = [], action) => {
   }
 };
 
+const showToast = (state = false, action) => {
+  switch (action.type) {
+    case FETCHING:
+      return true;
+    case STAT_BY_PROJECT:
+      return false;
+    default:
+      return state;
+  }
+};
+
 export default combineReducers({
   data,
+  showToast,
 });
