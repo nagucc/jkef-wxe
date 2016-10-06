31c396842bd92395975d2265d22a1d8bd026e285
na57 <na57@qq.com>
2016-10-06 19:54:37 +0800

    fix bugs

--- src/api/controllers/worker.js
+++ src/api/controllers/worker.js
@@ -2,15 +2,10 @@ import { acceptorManager } from '../../config';
 
 const interval = 60000;
 
-const statByProject = async () => {
+setInterval(async () => {
   await acceptorManager.computeStatByProject();
-  setTimeout(acceptorManager.computeStatByProject.bind(acceptorManager), interval);
-};
+}, interval);
 
-const statByYear = async () => {
+setInterval(async () => {
   await acceptorManager.computeStatByYear();
-  setTimeout(acceptorManager.computeStatByYear.bind(acceptorManager), interval);
-};
-
-statByProject();
-statByYear();
+}, interval);
