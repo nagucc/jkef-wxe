baab5a17eb701ab089664efb5685243c5118faf0
na57 <na57@qq.com>
2016-11-02 14:29:51 +0800

    update

--- src/api/controllers/wxe-auth.js
+++ src/api/controllers/wxe-auth.js
@@ -1,16 +1,20 @@
+/*
+eslint-disable no-console, no-param-reassign
+ */
+
 import { Router } from 'express';
 import api from 'wxent-api-redis';
 import { signin, getme, getUserId } from 'wxe-auth-express';
 import { wxentConfig as wxcfg, redisConfig as redis,
   host, profileMiddlewares, supervisorDpt, manageDpt } from '../../config';
-// import { getProfileByUserId, isManager, isSupervisor } from './middlewares';
 import { SUCCESS } from 'nagu-validates';
 
 const router = new Router();
 const wxapi = api(wxcfg.corpId, wxcfg.secret, wxcfg.agentId, redis.host, redis.port);
 const DEBUG = process.env.NODE_ENV === 'development';
 
-if (DEBUG) console.log('目前处于测试状态'); // eslint-disable-line no-console
+if (DEBUG) console.log('目前处于测试状态');
+
 router.get('/',
   signin({
     wxapi,
@@ -22,22 +26,25 @@ router.get('/me', getme());
 
 router.get('/me/roles',
   getUserId(),
+  // 判断当前用户是否是Supervisor
   profileMiddlewares.isSupervisor(
     req => req.user.userid,
     supervisorDpt,
     (isSupervisor, req, res, next) => {
-      res.roles = { isSupervisor }; // eslint-disable-line no-param-reassign
+      res.roles = { isSupervisor };
       next();
     },
   ),
+  // 判断当前用户是否是Manager
   profileMiddlewares.isManager(
     req => req.user.userid,
     manageDpt,
     (isManager, req, res, next) => {
-      res.roles = { ...res.roles, isManager }; // eslint-disable-line no-param-reassign
+      res.roles = { ...res.roles, isManager };
       next();
     },
   ),
+  // 返回结果
   (req, res) => res.send({ ret: SUCCESS, data: res.roles }),
 );
 
